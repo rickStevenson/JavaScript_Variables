b12afc130b8e0c52374277553a3d3aaba233c844
RickStevenson <dadsathome1@gmail.com>
2024-10-03 08:38:40 -0600

    Exercises

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Rick";
+lastName = "Stevenson";
+age = 53;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "M";
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(
+  "The value of hasRead is: " + hasRead
+);
